30c45f3c2ba477e9eae3f9336169c96e081fdbc3
jeevan <jeev5324u@gmail.con>
2024-04-17 11:49:08 +0530

    email note

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,9 +9,20 @@ def compile() {
 
 def unittest() {
  if (app_lang == "nodejs"){
-    sh 'npm test'
+    try {
+      sh 'npm test'
+    }catch(Exception e){
+      email("unit test failed")
+    }
  }
  if (app_lang == "java/maven"){
     sh 'mvn test'
  }
+ if (app_lang == "python"){
+    sh 'pyhton3 -m unittest'
+ }
+}
+
+def email(email_note){
+ println "(email_note)"
 }
